3730bd68df5e2ec89aa2cc72126771ebddcc47f9
Gabriel Henrique Lopes Vaciliev <ghvaciliev@agropecuariaipe.int>
2026-06-11 10:45:43 -0300

    Corrigindo erro no corpo do email

--- contato/contato.js
+++ contato/contato.js
@@ -7,7 +7,6 @@ form.addEventListener("submit", function (event) {
     const email = document.getElementById("email").value.trim();
     const mensagem = document.getElementById("mensagem").value.trim();
 
-    // Regex para validar e-mail
     const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (!regexEmail.test(email)) {
@@ -15,15 +14,14 @@ form.addEventListener("submit", function (event) {
         return;
     }
 
-    if (mensagem === "") {
-        alert("Por favor, digite uma mensagem.");
-        return;
-    }
-
     const assunto = "Contato pelo Portfólio";
 
     const corpo =
-        `E-mail do remetente: ${email}%0D%0A%0D%0A${mensagem}`;
+`E-mail do remetente: ${email}
+
+${mensagem}
+
+Atenciosamente,`;
 
     window.location.href =
         `mailto:gabrielvaciliev66@gmail.com?subject=${encodeURIComponent(assunto)}&body=${encodeURIComponent(corpo)}`;
